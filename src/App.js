c3f0b5552243d93c7c4cf6916ee1131815b42905
Md Mamun <juborajvai22@gmail.com>
2022-04-30 15:59:00 +0600

    banner complet

--- src/App.js
+++ src/App.js
@@ -4,10 +4,25 @@ import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Pages/Home/Header/Header';
+import Banner from './Pages/Home/Banner/Banner';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+import ManageItem from './Pages/Item/ManageItem/ManageItem';
+import AddItem from './Pages/Item/AddItem/AddItem';
+import MyItem from './Pages/Item/MyItem/MyItem';
+import Login from './Pages/Login/Login/Login';
 function App() {
   return (
     <div className="Apps">
       <Header></Header>
+     
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/manageItem' element={<ManageItem></ManageItem>}></Route>
+        <Route path='/addItem' element={<AddItem></AddItem>}></Route>
+        <Route path='/myItem' element={<MyItem></MyItem>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/Pages/Home/Header/Header.js
+++ src/Pages/Home/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
@@ -7,19 +8,19 @@ const Header = () => {
             <div className="nav-section bg-light">
                   <Navbar collapseOnSelect expand="lg" >
                         <Container>
-                              <Navbar.Brand href="#home">Walton Warehouse</Navbar.Brand>
+                              <Navbar.Brand as={Link} to='/'>Walton Warehouse</Navbar.Brand>
                               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                               <Navbar.Collapse id="responsive-navbar-nav">
                                     <Nav className="me-auto">
-                                         
-                                         
+
+
                                     </Nav>
                                     <Nav className='text-center'>
-                                          <Nav.Link >Manage Items</Nav.Link>
-                                          <Nav.Link >Add Item</Nav.Link>
-                                          <Nav.Link >My items</Nav.Link>
-                                          <Nav.Link >Login</Nav.Link>
-                                         
+                                          <Nav.Link as={Link} to='/manageItem' >Manage Items</Nav.Link>
+                                          <Nav.Link as={Link} to='/addItem' >Add Item</Nav.Link>
+                                          <Nav.Link as={Link} to='/myItem' >My items</Nav.Link>
+                                          <Nav.Link as={Link} to='/login' >Login</Nav.Link>
+
                                     </Nav>
                               </Navbar.Collapse>
                         </Container>
